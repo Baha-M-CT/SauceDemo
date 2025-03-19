292567d986a4eeee1123901771bac8bcf1966e7c
bm2701 <bm2701@exo.att.com>
2025-03-19 13:45:32 -0400

    add failed test logs to allure report

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -3,6 +3,7 @@ package base;
 import factory.DriverFactory;
 import io.qameta.allure.Allure;
 import org.apache.commons.io.FileUtils;
+import org.apache.log4j.Logger;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
@@ -14,11 +15,13 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.time.Duration;
 
 public class BaseTest {
 
     protected WebDriver driver;
+    private static final Logger logger = Logger.getLogger(BaseTest.class);
 
     public Object getDriver() {
         return driver;
@@ -28,15 +31,23 @@ public class BaseTest {
     public void setup() {
         driver = new DriverFactory().initializeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-
     }
 
     @AfterMethod
     public void afterMethod(ITestResult result) {
         if (ITestResult.FAILURE == result.getStatus()) {
             String testCaseName = result.getMethod().getMethodName();
+            logger.error("Test case failed: " + testCaseName, result.getThrowable());
             File destFile = new File("target" + File.separator + "screenshots" + File.separator + testCaseName + ".png");
             takeScreenShot(destFile);
+
+            // Attach logs to Allure report
+            String logFilePath = "target/logs/test.log";
+            try (InputStream logStream = new FileInputStream(logFilePath)) {
+                Allure.addAttachment("Test Logs", "text/plain", logStream, ".log");
+            } catch (IOException e) {
+                logger.error("Failed to attach logs to Allure report", e);
+            }
         }
         driver.quit();
     }
@@ -51,7 +62,5 @@ public class BaseTest {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
     }
-
 }
